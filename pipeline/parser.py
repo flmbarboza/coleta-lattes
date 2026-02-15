429f08fd0383bdc4e069f3c05bbaef63e1d274ec
Flavio Barboza <flmbarboza@gmail.com>
2026-02-15 12:04:09 -0300

    Update parser.py

--- pipeline/parser.py
+++ pipeline/parser.py
@@ -1,30 +1,23 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
-from pathlib import Path
 
-def extrair_artigos():
+def parse_xml(xml_bytes):
+
+    root = ET.fromstring(xml_bytes)
+
+    docente = root.attrib.get("NOME-COMPLETO")
 
     dados = []
 
-    for file in Path("data/xml").glob("*.xml"):
+    artigos = root.findall(".//ARTIGO-PUBLICADO")
 
-        tree = ET.parse(file)
-        root = tree.getroot()
+    for art in artigos:
+        info = art.find("DADOS-BASICOS-DO-ARTIGO")
 
-        docente = root.attrib.get("NOME-COMPLETO")
+        dados.append({
+            "docente": docente,
+            "titulo": info.attrib.get("TITULO-DO-ARTIGO"),
+            "ano": info.attrib.get("ANO-DO-ARTIGO")
+        })
 
-        artigos = root.findall(".//ARTIGO-PUBLICADO")
-
-        for art in artigos:
-            info = art.find("DADOS-BASICOS-DO-ARTIGO")
-
-            dados.append({
-                "docente": docente,
-                "titulo": info.attrib.get("TITULO-DO-ARTIGO"),
-                "ano": info.attrib.get("ANO-DO-ARTIGO")
-            })
-
-    df = pd.DataFrame(dados)
-    df.to_csv("data/processed/artigos.csv", index=False)
-
-    return df
+    return pd.DataFrame(dados)
