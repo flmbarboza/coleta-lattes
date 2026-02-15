ec23cfd55ebc751ce9d1c59b0e151eec9d823c02
Flavio Barboza <flmbarboza@gmail.com>
2026-02-15 12:04:55 -0300

    Update 1_Download_Lattes.py

--- pages/1_Download_Lattes.py
+++ pages/1_Download_Lattes.py
@@ -1,13 +1,25 @@
 import streamlit as st
 import pandas as pd
+from pipeline.parser import parse_xml
 from pipeline.downloader import baixar_lattes
 
-st.title("⬇️ Download dos Currículos Lattes")
+st.title("Download Lattes")
 
-df = pd.read_csv("data/docentes.csv")
+df_docentes = pd.read_csv("data/docentes.csv")
 
-st.dataframe(df)
+dados_totais = []
 
-if st.button("Baixar currículos"):
-    resultado = baixar_lattes(df)
-    st.write(resultado)
+if st.button("Coletar dados"):
+
+    for _, row in df_docentes.iterrows():
+
+        xml = baixar_lattes(row["id_lattes"])
+
+        if xml:
+            df = parse_xml(xml)
+            dados_totais.append(df)
+
+    if dados_totais:
+        st.session_state["artigos"] = pd.concat(dados_totais)
+
+        st.success("Dados carregados!")
